fcf1a3f95e61a5592b5f803980b14d0e44292abb
cldnjs <a74417407@gmail.com>
2017-07-26 14:30:32 +0900

    file upload처리 구현

--- Server/restful/file.js
+++ Server/restful/file.js
@@ -2,7 +2,21 @@ const express = require('express');
 const uuid = require('node-uuid');
 const router = express.Router();
 const upload = require('../app.js');
+const mysql = require('../support/mysql.js');
 
 router.route('/uploadfile').post((req, res) => {
-    req.file.uuid = uuid.v4();
+    const path = req.file.path;
+    const uuid = uuid.v1();
+
+    mysql.query(`INSERT INTO file_manager VALUES('${uuid}', '${path}')`, (err, rows) => {
+        if(err) {
+            console.log(err);
+            res.sendStatus(204);
+        } else {
+            console.log('uuid와 파일 경로가 DB에 저장되었습니다');
+            res.sendStatus(200);
+        }
+    })
+    
+    res.send(uuid);
 });
