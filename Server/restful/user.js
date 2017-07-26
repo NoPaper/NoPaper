c682f5fa31c2e92c6f76a4e5e24daebd40a4d24d
cldnjs <a74417407@gmail.com>
2017-07-26 11:19:26 +0900

    user.js 삭제 후 sample.js를 user.js로 이름 수정

--- Server/restful/user.js
+++ Server/restful/user.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('../support/mysql.js');
+const mysql = require('./support/mysql.js');
 
 router.route('').post((req, res) => {
-    
+
 });
 
 router.route('login/teacher').post((req, res) => {
     // DB에 쿼리 날려서 로우 뜨면 성공(고유 키 포함)
     const id = req.body.id
     const password = req.body.password 
-    const key = req.body.key
 
     mysql.query(`SELECT ${id}, ${password} FROM account_teacher`, (err, rows) => {
         if(rows.length != 0) {
-            mysql.query(`SELECT ${key} from teacher_secret`, (err, rows) => {
-                if(rows.length != 0) {
-                    res.sendStatus(200)
-                } else {
-                    res.sendStatus(204)
-                }
-            });
+            console.log(rows);
+            res.sendStatus(200)
         } else {
-            res.sendStatus(204);
+            res.sendStatus(204)
         }
     });
 });
@@ -45,21 +39,22 @@ router.route('signup/teacher').post((req, res) => {
     // DB에 쿼리 날려서 로우가 없다면 성공
     const id = req.body.id
     const password = req.body.password
+    const key = req.body.key
 
     mysql.query(`SELECT ${id} FROM account_teacher`, (err, rows) => {
-        if(rows.lenth != 0) {
-            res.sendStatus(204)
-        } else {
-            mysql.query(`INSERT INTO account_teacher VALUES(${id}, ${password})`, (err, rows) => {
+       if(rows.length != 0) {
+           res.sendStatus(204)
+       } else {
+           mysql.query(`INSERT INTO account_teacher VLAUES(${id}, ${password}, ${key})`, (err, rows) => {
                if(err) {
                    console.log(err);
                    res.sendStatus(204)
                } else {
-                   console.log('DB에 정보 저장 성공');
+                   console.log('DB에 정보들이 성공적으로 저장되었습니다');
                    res.sendStatus(200)
                }
-            });
-        }
+           });
+       }
     });
 });
 router.route('signup/student').post((req, res) => {
@@ -70,7 +65,7 @@ router.route('signup/student').post((req, res) => {
     const classs = req.body.class
 
     mysql.query(`SELECT ${id} FROM account_student`, (err, rows) => {
-        if(rows.length != 0) {
+        if(rows.length ! =0) {
             res.sendStatus(204)
         } else {
             mysql.query(`INSERT INTO account_teacher VALUES(${id}, ${password}, ${grade}, ${classs})`, (err, rows) => {
