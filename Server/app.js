6c446265a04e7c5ee221c87c1c2e812160556cb7
JoMingyu <city7310@naver.com>
2017-07-26 09:32:03 +0900

    app.js initialize, sample.js 추가

---
+++ Server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const session = require('express-session');
+app.use(session({
+    secret: '2ka@>.!!ask',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/', require('./restful/sample.js'))
+
+app.listen(3003, () => {
+    console.log('Server is listening on 3003');
+});
