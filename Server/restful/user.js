803873c3e1f693a29b50d57d61850d45107fa6a3
JoMingyu <city7310@naver.com>
2017-07-26 11:22:09 +0900

    쿼리문 교체와 에러 fix

--- Server/restful/user.js
+++ Server/restful/user.js
@@ -1,36 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('./support/mysql.js');
+const mysql = require('../support/mysql.js');
 
 router.route('').post((req, res) => {
 
 });
 
-router.route('login/teacher').post((req, res) => {
+router.route('/login/teacher').post((req, res) => {
     // DB에 쿼리 날려서 로우 뜨면 성공(고유 키 포함)
-    const id = req.body.id
-    const password = req.body.password 
+    const id = req.body.id;
+    const password = req.body.password;
 
-    mysql.query(`SELECT ${id}, ${password} FROM account_teacher`, (err, rows) => {
+    mysql.query(`SELECT * FROM account_teacher WHERE id='${id}' AND password='${password}'`, (err, rows) => {
         if(rows.length != 0) {
-            console.log(rows);
-            res.sendStatus(200)
+            res.sendStatus(200);
         } else {
-            res.sendStatus(204)
+            res.sendStatus(204);
         }
     });
 });
 router.route('login/student').post((req, res) => {
     // DB에 쿼리 날려서 로우있으면 성공
-    const id = req.body.id
-    const password = req.body.password
+    const id = req.body.id;
+    const password = req.body.password;
 
-    mysql.query(`SELECT ${id}, ${password} FROM account_stduent`, (err, rows) => {
+    mysql.query(`SELECT * FROM account_stduent WHERE id='${id}' AND password='${password}'`, (err, rows) => {
         if(rows.length != 0) {
-            console.log(rows);
-            res.sendStatus(200)
+            res.sendStatus(200);
         } else {
-            res.sendStatus(204)
+            res.sendStatus(204);
         }
     });
 });
@@ -65,7 +63,7 @@ router.route('signup/student').post((req, res) => {
     const classs = req.body.class
 
     mysql.query(`SELECT ${id} FROM account_student`, (err, rows) => {
-        if(rows.length ! =0) {
+        if(rows.length != 0) {
             res.sendStatus(204)
         } else {
             mysql.query(`INSERT INTO account_teacher VALUES(${id}, ${password}, ${grade}, ${classs})`, (err, rows) => {
