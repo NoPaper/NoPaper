21dfc9f6b5e199439c5d4da81d3639dbdbc0ca97
JoMingyu <city7310@naver.com>
2017-07-26 19:59:13 +0900

    이미지 자르고 signup guide xml 완료, splash 레이아웃 추가

--- NoPaper/app/src/main/java/com/planb/nopaper/activities/MainActivity.java
+++ NoPaper/app/src/main/java/com/planb/nopaper/activities/MainActivity.java
@@ -1,11 +1,11 @@
 package com.planb.nopaper.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.planb.nopaper.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
