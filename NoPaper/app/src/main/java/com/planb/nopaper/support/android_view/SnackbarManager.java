3f96593e71b05f395c4a12a3a6243ce3ce940dc9
JoMingyu <city7310@naver.com>
2017-07-26 18:39:25 +0900

    SnackbarManager, DBHelper

--- NoPaper/app/src/main/java/com/planb/nopaper/support/android_view/SnackbarManager.java
+++ NoPaper/app/src/main/java/com/planb/nopaper/support/android_view/SnackbarManager.java
@@ -1,8 +1,19 @@
 package com.planb.nopaper.support.android_view;
 
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
 /**
  * Created by dsm2016 on 2017-07-26.
  */
 
 public class SnackbarManager {
+    public static Snackbar make(View v, String msg) {
+        return Snackbar.make(v, msg, Snackbar.LENGTH_SHORT).setAction("확인", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.GONE);
+            }
+        });
+    }
 }
