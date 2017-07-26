20cf07edd6b15b54c7e51c3fd16e49b6d97e3dec
cldnjs <a74417407@gmail.com>
2017-07-26 15:04:29 +0900

    클라이언트로 파일 보내주기 구현

--- Server/restful/file.js
+++ Server/restful/file.js
@@ -3,6 +3,7 @@ const uuid = require('node-uuid');
 const router = express.Router();
 const upload = require('../app.js');
 const mysql = require('../support/mysql.js');
+const fs = require('fs');
 
 router.route('/uploadfile').post(upload.single('beacon_file'), (req, res) => {
     const filename = req.file.filename;
@@ -15,6 +16,18 @@ router.route('/uploadfile').post(upload.single('beacon_file'), (req, res) => {
         } else {
             console.log('uuid와 파일 경로가 DB에 저장되었습니다');
             res.status(200).send(v1);
+        }
+    });
+});
+
+router.route('/download').post((req, res) => {
+    const id = req.body.id;
+    mysql.query(`SELECT file_path FROM file_manager WHERE id='${id}'`, (err, rows) => {
+        if(rows.length == 0) {
+            res.sendStatus(204);
+        } else {
+            fs.readFile(rows[0].file_path);
+            res.sendfile(rwos[0].file_path);
         }
     });
 });
